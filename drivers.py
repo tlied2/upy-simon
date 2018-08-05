67d78c2d05409b359b346c4c8c3f15f12b794d5d
tlied2 <34370982+tlied2@users.noreply.github.com>
2018-08-05 00:19:52 -0400

    Code fixes, now working

--- drivers.py
+++ drivers.py
@@ -1,24 +1,33 @@
+''' Drivers for Simon game'''
+
 import machine
+import utime as time
 
 
-class driver:
+class LEDBTN(object):
+    ''' LED Button driver using mcp23008 '''
+
+    BASE_ADDR = 0x20
+
+    MCP23008_IODIR = 0x00
+    MCP23008_IPOL = 0x01
+    MCP23008_GPINTEN = 0x02
+    MCP23008_DEFVAL = 0x03
+    MCP23008_INTCON = 0x04
+    MCP23008_IOCON = 0x05
+    MCP23008_GPPU = 0x06
+    MCP23008_INTF = 0x07
+    MCP23008_INTCAP = 0x08
+    MCP23008_GPIO = 0x09
+    MCP23008_OLAT = 0x0A
+
+    buttondict = {'b': 0b00001000,
+                  'g': 0b00000100,
+                  'y': 0b00000010,
+                  'r': 0b00000001}
 
     def __init__(self):
         self.bus = machine.I2C(scl=machine.Pin(5), sda=machine.Pin(4))
-
-        self.BASE_ADDR = 0x20
-
-        self.MCP23008_IODIR = 0x00
-        self.MCP23008_IPOL = 0x01
-        self.MCP23008_GPINTEN = 0x02
-        self.MCP23008_DEFVAL = 0x03
-        self.MCP23008_INTCON = 0x04
-        self.MCP23008_IOCON = 0x05
-        self.MCP23008_GPPU = 0x06
-        self.MCP23008_INTF = 0x07
-        self.MCP23008_INTCAP = 0x08
-        self.MCP23008_GPIO = 0x09
-        self.MCP23008_OLAT = 0x0A
 
         # Enable outputs, bits 4-7
         self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_IODIR, 0b00001111]))
@@ -29,25 +38,55 @@ class driver:
         # Enable pullups, bits 0-3
         self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_GPPU, 0b00001111]))
 
-        self.buttondict = {'r': (0b00001000, 0b10000000),
-                           'y': (0b00000100, 0b01000000),
-                           'g': (0b00000010, 0b00100000),
-                           'b': (0b00000001, 0b00010000)}
+        # Turn off all lights
+        self.all_off()
 
-    def lighton(self, code):
-        val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0x0F
-        newval = val << 4 & self.buttondict[code][1]
-        print(newval)
+    def all_off(self):
+        ''' Turns all lights off '''
+
+        self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_GPIO, 0x0]))
+
+    def all_on(self):
+        ''' Turns all lights on '''
+
+        self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_GPIO, 0xF0]))
+
+    def light_on(self, code):
+        ''' Turns on a single light '''
+
+        print("Called lighton for %s" % code)
+        val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0xF0
+        print("Orig Value: %s" % hex(val))
+        newval = val | self.buttondict[code] << 4
+        print("New Value: %s" % hex(newval))
         self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_GPIO, newval]))
 
-    def lightoff(self, code):
-        val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0x0F
-        newval = (val << 4 ^ self.buttondict[code][1])
-        print(newval)
+    def light_off(self, code):
+        ''' Turns off a single light '''
+
+        print("Called lightoff for %s" % code)
+        val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0xF0
+        print("Orig Value: %s" % hex(val))
+        newval = val ^ self.buttondict[code] << 4
+        print("New Value: %s" % hex(newval))
         self.bus.writeto(self.BASE_ADDR, bytearray([self.MCP23008_GPIO, newval]))
 
-    def getpressed(self, code):
-        val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0x0F
-        if val & self.buttondict[code][0] == val:
-            return True
+    def get_pressed(self):
+        ''' Returns pressed button after it is released '''
+
+        print("Waiting for button press")
+        while True:
+            val = self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0x0F
+            if val != 0:
+                print("Button read val: %s" % hex(val))
+                break
+
+        print("Waiting for button release")
+        while self.bus.readfrom_mem(self.BASE_ADDR, self.MCP23008_GPIO, 1)[0] & 0x0F == val:
+            time.sleep(0.1)
+
+        for key in self.buttondict:
+            if val == self.buttondict[key]:
+                return key
+        print("This should never happen")
         return False
--- main.py
+++ main.py
@@ -1,125 +1,98 @@
-import machine
-import time
+''' Simon game implementation '''
+
+import utime as time
 import urandom
-import drivers
 
-driver = drivers.driver()
-
-
-def pick_item(sequence):
-    div = 0x3fffffff // len(sequence)
-    return sequence[urandom.getrandbits(30) // div]
+from drivers import LEDBTN
 
 
-class Button:
+class Simon(object):
+    ''' Main Simon game object '''
 
-    def __init__(self, code):
-        self.code = code
+    buttons = ['r', 'y', 'g', 'b']
 
-    def turnon(self, seconds=0):
-        driver.lighton(self.code)
-        time.sleep(seconds)
-
-    def turnoff(self, seconds=0):
-        driver.lightoff(self.code)
-        time.sleep(seconds)
-
-    def isPressed(self):
-        driver.getpressed(self.code)
-        return
-
-
-class Simon:
-
-    def __init__(self):
-        self.red = Button('r')
-        self.yellow = Button('y')
-        self.green = Button('g')
-        self.blue = Button('b')
-
-        self.buttons = {'r': self.red, 'y': self.yellow, 'g': self.green, 'b': self.blue}
-
+    def __init__(self, driver):
+        self.driver = driver
         self.score = 0
-        self.button_seq = pick_item('rgby')
-        self.button_cur = self.button_seq[0]
-        self.button_place = 0
+        self.button_seq = []
 
-    def blink(self, button, time_on=1, time_off=0.2):
-        button.turnon(time_on)
-        button.turnoff(time_off)
+    def pick_color(self):
+        ''' Returns a random color '''
+        div = 0x3fffffff // len(self.buttons)
+        return self.buttons[urandom.getrandbits(30) // div]
+
+    def blink(self, color, time_on=1, time_off=0.2):
+        ''' Blink light identified by color with timings specified '''
+        self.driver.light_on(color)
+        time.sleep(time_on)
+        self.driver.light_off(color)
+        time.sleep(time_off)
 
     def blink_all(self, time_on=1, time_off=0.2):
-        for button in self.buttons.values():
-            button.turnon(0)
+        ''' Blink all lights with timings specified '''
+        self.driver.all_on()
         time.sleep(time_on)
-        for button in self.buttons.values():
-            button.turnoff(0)
+        self.driver.all_off()
         time.sleep(time_off)
 
     def fail_game(self):
-
+        ''' Wrong button pressed, clear everything and flash lights '''
         self.score = 0
-        self.button_seq = pick_item('rgby')
-        self.button_cur = self.button_seq[0]
-        self.button_place = 0
+        self.button_seq = []
 
-        self.blink_all()
-        self.blink_all()
-
-        self.game_loop()
-
-    def advance_color(self):
-
-        if self.button_place + 1 == len(self.button_seq):
-            # sequence completed, add next color and restart from beginning
-            self.button_seq += pick_item('rgby')
-            self.button_place = 0
-            self.score += 1
-            self.button_cur = self.button_seq[0]
-            self.game_loop()
-        else:
-            self.button_place += 1
-            self.button_cur = self.button_seq[self.button_place]
-
-    def get_button_press(self):
-        code = None
-        for button in self.buttons.values():
-            if button.isPressed():
-                code = button.code
-        return code
+        for idx in range(3):
+            self.blink_all()
 
     def game_loop(self):
+        ''' Main game loop '''
+        failed = False
 
-        time.sleep(1)
-        self.do_sequence()
+        while not failed:
+            time.sleep(2)
 
-        while True:
+            self.button_seq += self.pick_color()
+            self.do_sequence()
 
-            code = self.get_button_press()
-            if code is not None:
-                if code == self.button_cur:
-                    while self.buttons[code].isPressed():
-                        time.sleep(0.1)
-                    self.advance_color()
-                    self.blink(self.buttons[code], 0.5, 0)
+            for color in self.button_seq:
+                gotcolor = self.driver.get_pressed()
+
+                if gotcolor == color:
+                    self.blink(color, 0.5, 0)
                 else:
-                    while self.buttons[code].isPressed():
-                        time.sleep(0.1)
                     self.fail_game()
+                    failed = True
+                    break
 
     def do_sequence(self):
-
+        ''' Displays current color sequence '''
         for color in self.button_seq:
-            self.blink(self.buttons[color])
+            print("Do sequence, color: %s" % color)
+            self.blink(color)
 
-    def game_start(self):
-        for button in self.buttons.values():
-            button.turnon(seconds=1)
-        for button in self.buttons.values():
-            button.turnoff(seconds=0)
-        self.game_loop()
+    def welcome(self):
+        ''' Welcome light show '''
+        for button in self.buttons:
+            self.driver.light_on(button)
+            time.sleep(1)
+        self.driver.all_off()
 
 
-game = Simon()
-print('game start')
-game.game_start()
+def main():
+    ''' Main entry point '''
+    driver = LEDBTN()
+    game = Simon(driver)
+
+    print('Welcome Lights')
+    game.welcome()
+
+    while True:
+        # Wait for keypress to start game
+        driver.get_pressed()
+
+        print('Game Start')
+        game.game_loop()
+        print("Game finished, starting new game")
+
+
+if __name__ == '__main__':
+    main()
